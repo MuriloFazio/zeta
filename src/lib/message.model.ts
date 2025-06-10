b043b775e65a1650df96b660565e02abcdbb4b69
Murilo <faziomurilo@gmail.com>
2025-06-10 16:31:57 -0300

    feat: add new message model to lib

---
+++ src/lib/message.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface Message{
+  userId: string;
+  model: "gpt-4" | "claude" | "gemini";
+  role: "user" | "assistant" | "system";
+  content: string;
+  createdAt: Date;
+}
+
+const MessageSchema = new Schema<Message>({
+  userId: { type: String, required: true },
+  model: {
+    type: String,
+    enum: ["gpt-4", "claude", "gemini"],
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "assistant", "system"],
+    required: true,
+  },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const Message =
+  mongoose.models.Message || mongoose.model<Message>("Message", MessageSchema);
